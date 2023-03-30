b15c02a412b697e99f15b18fbeb252fd3e7bbdc5
Antoine BAURI <antoine.bauri@therasoft.com>
2023-03-30 09:13:54 +0200

    wip

--- server/manifest.js
+++ server/manifest.js
@@ -50,7 +50,8 @@ module.exports = new Confidence.Store({
                                 host: process.env.DB_HOST || '0.0.0.0',
                                 user: process.env.DB_USER || 'root',
                                 password: process.env.DB_PASSWORD || 'hapi',
-                                database: process.env.DB_DATABASE || 'user'
+                                database: process.env.DB_DATABASE || 'user',
+                                port: process.env.DB_PORT || 3406
                             },
                             migrations: {
                                 stub: Schwifty.migrationsStubPath
